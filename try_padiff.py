ff6b2c4e68e8188cb0b6aedf9123b4288d28627a
jiamingkong <jiamingkong@users.noreply.github.com>
2023-03-24 19:16:34 +0800

    Added PaDiff script

--- try_padiff.py
+++ try_padiff.py
@@ -5,6 +5,7 @@ from wavlm import WavLM, WavLMConfig
 
 config = WavLMConfig()
 layer = WavLM(config)
+layer.load_dict(paddle.load("wavlm-base-plus/wavlm-base-paddle.pdparams"))
 layer.eval()
 
 from unilm.WavLM import WavLM, WavLMConfig
@@ -13,11 +14,11 @@ from unilm.WavLM import WavLM, WavLMConfig
 checkpoint = torch.load('./wavlm-base-plus/WavLM-Base-Official.pt')
 cfg = WavLMConfig(checkpoint['cfg'])
 module = WavLM(cfg)
-# module.load_state_dict(checkpoint['model'])
+module.load_state_dict(checkpoint['model'])
 module.eval()
 
 inp = paddle.rand((1, 16000)).numpy().astype("float32")
 inp = ({'x': paddle.to_tensor(inp)},
      {'x': torch.as_tensor(inp)})
 
-auto_diff(layer, module, inp, auto_weights=True, options={'atol': 1e-2, 'rtol':1e-2, 'compare_mode': 'strict', 'single_step':True, "diff_phase": "forward"})
+auto_diff(layer, module, inp, auto_weights=False, options={'atol': 1e-1, 'rtol':1e-2, 'compare_mode': 'strict', 'single_step':True, "diff_phase": "forward"})
